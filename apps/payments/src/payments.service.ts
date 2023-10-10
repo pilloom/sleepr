224cfabb623a0cfd93ca968b8914ff700198df9b
José Antonio Rodríguez Barceló <rbsistemas@hotmail.com>
2023-10-10 14:33:01 -0600

    feat: :sparkles: 22 Emit Notification
    Se crea la aplicacion para notificar por email, se refactorizan algunos DTOs para incluir el correo, se crea el microservicio y se conecta reservas al microservicio

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Stripe } from 'stripe';
-import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
   private readonly stripe = new Stripe(
     this.configService.get('STRIPE_SECRET_KEY'),
@@ -14,18 +20,10 @@ export class PaymentsService {
     },
   );
 
-  // Se comenta el codigo por el fallo al enviar el numero de tarjeta directamente
-  async createCharge({ /* card,  */ amount }: CreateChargeDto) {
-    /* const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    }); */
-
+  async createCharge({ /* card,  */ amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
-      /* payment_method: paymentMethod.id, */
       amount: amount * 100,
       confirm: true,
-      /* payment_method_types: ['card'], */
       currency: 'usd',
       payment_method: 'pm_card_visa',
       automatic_payment_methods: {
@@ -33,6 +31,8 @@ export class PaymentsService {
         enabled: true,
       },
     });
+
+    this.notificationsService.emit('notify_email', { email });
 
     return paymentIntent;
   }
